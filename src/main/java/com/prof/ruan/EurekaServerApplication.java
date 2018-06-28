49e83b048508562000109c4708e4aea18fa8d70c
ProfRuan <1687255679@qq.com>
2018-06-28 08:23:10 +0800

    init codes

---
+++ src/main/java/com/prof/ruan/EurekaServerApplication.java
@@ -0,0 +1,20 @@
+package com.prof.ruan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * 添加@EnableEurekaServer注解
+ * SpringCloud微服务的注册中心
+ * @author profruan
+ *
+ */
+@EnableEurekaServer
+@SpringBootApplication
+public class EurekaServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EurekaServerApplication.class, args);
+	}
+}
